bba922a720a818b4c0de88800af2a1333efdb10d
Alok Kumar <alok61.bd@gmail.com>
2021-10-31 17:03:41 +0600

    packages edited

--- src/components/Home/HomePakcage/HomePackage.js
+++ src/components/Home/HomePakcage/HomePackage.js
@@ -6,7 +6,7 @@ import './HomePackage.css'
 const HomePackage = (props) => {
     const {addToLocal}=useLocalStore();
     const history=useHistory();
-    const {_id,package_name,image,Duration,Routes,Price}=props.tourPackage;
+    const {_id,packageName,image,Duration,description,Price}=props.tourPackage;
 
     const handlebook=(id)=>{
         history.push(`/placebooking/${id}`)
@@ -18,13 +18,12 @@ const HomePackage = (props) => {
             <img src={image} alt="" />
             <div className="info">
                <div className="row">
-               <h3 className="col-8 col-lg-12">{package_name}</h3>
-               <p className="col-4 col-lg-4">price : {Price}</p>
+               <h3 className="col-8 col-lg-12">{packageName}</h3>
+               <p className="col-4 col-lg-12">price : {Price}</p>
+               <p className=" col-6 col-lg-12">Duration: {Duration}</p>
+               <p className="col-12">{description.slice(0,200)}</p>
                </div>
-               <div className="d-flex justify-content-between align-items-center">
-               <p className="">duration :1night/2day </p>
-                <p className="">Routes : {Routes}</p>
-               </div>
+              
                 
                 <button onClick={()=>handlebook(_id)}>Book now</button>
             </div>
